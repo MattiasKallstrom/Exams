9b7c4aec8450fe80870b4ab4041bcb0f3383391e
Mattias <mattias-kallstrom@hotmail.com>
2022-01-16 01:16:27 +0100

    123

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import './default.css';
 import { auth, userProfile } from './firebase/utils';
-import {  BrowserRouter as  Switch, Route, Router, Redirect } from "react-router-dom";
+import {  BrowserRouter as  Switch, Route, Router, Routes, Redirect } from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 //Pages
@@ -60,7 +60,7 @@ class App extends Component {
       <div className="App">
         <div className="">
         <Router basename="/Exams">
-          <Routes>
+          <BrowserRouter>
           <Switch>
            <Route exact path ="/" render={() => (
              <MainLayout loggedIn={loggedIn}>
@@ -90,7 +90,7 @@ class App extends Component {
              </MainLayout>
            )} />
           </Switch>
-          </Routes>
+          </BrowserRouter>
           </Router>
         </div>
       </div>
