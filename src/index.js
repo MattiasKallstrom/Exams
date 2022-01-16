3601e6de82f88adc857b6e45234c62e07b43a498
Mattias <mattias-kallstrom@hotmail.com>
2022-01-17 00:19:13 +0100

    Routing fixed hopefully

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import { BrowserRouter } from 'react-router-dom';
 import './default.css';
 
 const app = (
-  <BrowserRouter basename="/Exams">
+  <BrowserRouter>
     <App />
   </BrowserRouter>
 );
